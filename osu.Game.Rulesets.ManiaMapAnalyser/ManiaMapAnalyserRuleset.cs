92f838919bdb7eb0c05e10290c4064763747d96d
2710165659 <2710165659@qq.com>
2026-07-09 21:11:57 +0800

    fix: 修复tachyon709更新引起的错误

--- osu.Game.Rulesets.ManiaMapAnalyser/ManiaMapAnalyserRuleset.cs
+++ osu.Game.Rulesets.ManiaMapAnalyser/ManiaMapAnalyserRuleset.cs
@@ -159,13 +159,13 @@ public partial class ManiaMapAnalyserRuleset : Ruleset
         {
         }
 
-        protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills, double clockRate)
+        protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills)
             => new DifficultyAttributes(mods, 0);
 
-        protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, double clockRate)
+        protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, Mod[] mods)
             => Array.Empty<DifficultyHitObject>();
 
-        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods, double clockRate)
+        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods)
             => Array.Empty<Skill>();
     }
 
